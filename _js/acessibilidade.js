756e51bc5e78e0f2cf6d383a1df5de9b1709ac10
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-08-15 12:20:10 -0300

    Update acessibilidade.js

--- _js/acessibilidade.js
+++ _js/acessibilidade.js
@@ -1,12 +1,12 @@
 function aumentarFonte() {
-    let corpo = document.querySelector('body');
+    let corpo = document.querySelector('.acessibilidade');
     let tamanhoAtual = window.getComputedStyle(corpo).fontSize;
     let novoTamanho = parseFloat(tamanhoAtual) + 2; // Aumenta o tamanho em 2px
     corpo.style.fontSize = novoTamanho + 'px';
 }
 
 function diminuirFonte() {
-    let corpo = document.querySelector('body');
+    let corpo = document.querySelector('.acessibilidade');
     let tamanhoAtual = window.getComputedStyle(corpo).fontSize;
     let novoTamanho = parseFloat(tamanhoAtual) - 2; // Diminui o tamanho em 2px
     corpo.style.fontSize = novoTamanho + 'px';
