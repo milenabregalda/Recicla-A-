2714f03bc3b2f9c13b7c591210763d538c3fe82d
Milena Bregalda <milenabregaldasenac@gmail.com>
2024-08-16 12:06:52 -0300

    add carousel images

---
+++ _js/carrossel.js
@@ -0,0 +1,37 @@
+// Lista de imagens a serem exibidas
+let vtBanner = ["_imgs/eletronico.jpg", "_imgs/eletronico2.png", "../_imgs/eletronico3.png"];
+let max = vtBanner.length - 1;
+let i = 0;
+
+$("#btnAnte").prepend($('<img>', {/* src: '../Imagens/arrowleft.svg' */}));
+$("#btnProx").prepend($('<img>', {/* src: '../Imagens/arrowright.svg' */}));
+// quando a gente chama o css no js, as propriedades que tem palavras compostas tira o hifen e coloca a letra maiuscula, colocando a palabra em camelCase.
+$("#banner").css("backgroundImage", "url(" + vtBanner[0] + ")");
+
+$("#btnAnte").click(function () {
+    troca(-1);
+})
+
+$("#btnProx").click(function () {
+    troca(1);
+})
+setInterval(() => troca(1), 4000)
+
+function troca(opr) {
+    $("#banner").css("backgroundImage", "url(" + vtBanner[i] + ")").fadeIn(1000, function () {
+        i += opr;
+        if (i > max) {
+            i = 0;
+        } else if (i < 0) {
+            i = max;
+        }
+        $("#banner").css("backgroundImage", "url(" + vtBanner[i] + ")").fadeIn(1000);
+        $("#b" + i).prop("checked", true);
+    });
+}
+
+$("input[name='banners']").click(function () {
+    let getNumber = $("input[name='banners']:checked").val()
+    console.log(getNumber)
+    $("#banner").css("backgroundImage", "url(" + vtBanner[getNumber] + ")").fadeIn(1000);
+})
