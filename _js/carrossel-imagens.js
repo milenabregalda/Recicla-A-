422e427eef8cf845cff87e3f506a985e4e611754
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-08-21 10:23:45 -0300

    Update carrossel-imagens.js

--- _js/carrossel-imagens.js
+++ _js/carrossel-imagens.js
@@ -1,42 +1,42 @@
-$(document).ready(function() {
+$(document).ready(() => {
     var executar = true;
     var idIntervalo;
     const banners = $(".banner-image");
     var bannerAtivo = 0;
 
     function troca(opr) {
-        if(executar) {
+        if (executar) {
             executar = false;
 
-            switch(opr) {
+            switch (opr) {
                 case false:
                     {
-                        clearInterval(idIntervalo);                         // Encerrar o loop de troca q já está rodando no fundo
-                        idIntervalo = setInterval( (() => troca(true)), 7000);    // Iniciar um novo loop para trocar de imagens
+                        clearInterval(idIntervalo);                                 // Encerrar o loop de troca que já está rodando no fundo
+                        idIntervalo = setInterval((() => troca(true)), 7000);      // Iniciar um novo loop para trocar de imagens
 
                         banners.eq(bannerAtivo).fadeOut(500, () => {
-                            if(bannerAtivo == 0)
-                                bannerAtivo = banners.length -1;
+                            if (bannerAtivo == 0)
+                                bannerAtivo = banners.length - 1;
                             else
                                 bannerAtivo--;
 
-                            banners.eq(bannerAtivo).fadeIn(500, (() => { executar = true; }) );
+                            banners.eq(bannerAtivo).fadeIn(500, (() => { executar = true; }));
                         });
-                        
+
                         break;
                     }
                 case true:
                     {
-                        clearInterval(idIntervalo);                         // Encerrar o loop de troca q já está rodando no fundo
-                        idIntervalo = setInterval( (() => troca(true)), 7000);    // Iniciar um novo loop para trocar de imagens
+                        clearInterval(idIntervalo);                                 // Encerrar o loop de troca que já está rodando no fundo
+                        idIntervalo = setInterval((() => troca(true)), 7000);      // Iniciar um novo loop para trocar de imagens
 
                         banners.eq(bannerAtivo).fadeOut(500, () => {
-                            if(bannerAtivo == banners.length -1)
+                            if (bannerAtivo == banners.length - 1)
                                 bannerAtivo = 0;
                             else
                                 bannerAtivo++;
 
-                            banners.eq(bannerAtivo).fadeIn(500, (() => { executar = true; }) );
+                            banners.eq(bannerAtivo).fadeIn(500, (() => { executar = true; }));
                         });
 
                         break;
@@ -46,7 +46,7 @@ $(document).ready(function() {
     }
 
     // Timer inicial
-    idIntervalo = setInterval( (() => troca(true)), 7000); // Iniciar o loop para trocar de imagens
+    idIntervalo = setInterval((() => troca(true)), 7000); // Iniciar o loop para trocar de imagens
 
     // Botão Esquerdo
     $("#btnAnte").click(() => troca(false));
